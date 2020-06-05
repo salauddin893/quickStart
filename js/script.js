70607e9ee2a227c7ca0e362eb467b28794ac839e
salauddin893 <sala6900@gmail.com>
2020-06-05 12:00:35 +0600

    quick start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function ($) {
+    "use strict";
+
+    // meanmenu
+    $('#mobile-menu').meanmenu({
+        meanMenuContainer: '.mobile-menu',
+        meanScreenWidth: "991"
+    });
+
+})(jQuery);
